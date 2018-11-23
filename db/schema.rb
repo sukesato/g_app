018d2326a5a52b02d987537e5618f314fe615318
s.sato <samurai310@aol.jp>
2018-11-23 11:26:23 +0000

    Device AM blogモデルとメイラー作成前 contact削除済

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181123103702) do
+ActiveRecord::Schema.define(version: 20181123112227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,14 +25,6 @@ ActiveRecord::Schema.define(version: 20181123103702) do
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.integer "blog_id"
-  end
-
-  create_table "contacts", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "labels", force: :cascade do |t|
