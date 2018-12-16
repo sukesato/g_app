d6809bd9dcbdfeaaed9ccf6a69fd12c13cc17cff
s.sato <samurai310@aol.jp>
2018-12-16 11:47:50 +0000

    SMTPサーバー構築

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # UserMailer.contact_mail(@contact).deliver
+      UserMailer.user_mail(@user).deliver
       # 保存の成功した場合の処理
       redirect_to user_path(@user.id), notice: "ユーザーを作成しました！"
     else
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'safe-beach-68305.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'glacial-anchorage-23676.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
