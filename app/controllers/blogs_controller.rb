5fd02ba59753966dc0e8cc348802d7cfa301dd2c
s.sato <samurai310@aol.jp>
2018-12-17 07:32:48 +0000

    優先度表示を数字から英語へ変更＆投稿一覧表示をログインユーザーのみに

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -11,38 +11,38 @@ class BlogsController < ApplicationController
   
   def filter
      #インスタンス変数3種類に入れる。ソートの種類ごとに
-    @high_blog = Blog.where(priority: 0).page(params[:page]).per(3)
+    @high_blog = Blog.where(priority: 0).where(user_id:current_user.id).page(params[:page]).per(3)
     if params[:id] == "A"
       @blogs = @high_blog
       @index_title = "優先度Aの投稿"
       render 'index'
     elsif params[:id] == "B"
-      @middle_blog= Blog.where(priority: 1).page(params[:page]).per(3)
+      @middle_blog= Blog.where(priority: 1).where(user_id:current_user.id).page(params[:page]).per(3)
       @blogs = @middle_blog
       @index_title = "優先度Bの投稿"
       render 'index'
     elsif params[:id] == "C"
-      @low_blog = Blog.where(priority: 2).page(params[:page]).per(3)
+      @low_blog = Blog.where(priority: 2).where(user_id:current_user.id).page(params[:page]).per(3)
       @blogs = @low_blog
       @index_title = "優先度Cの投稿"
       render 'index'
     elsif params[:id] == "nonfin"
-      @nonfin_blog = Blog.where(task_flag: false).page(params[:page]).per(3)
+      @nonfin_blog = Blog.where(task_flag: false).where(user_id:current_user.id).page(params[:page]).per(3)
       @blogs = @nonfin_blog
       @index_title = "未完了の投稿"
       render template: "blogs/nf_index"
     elsif params[:id] == "nonfin_A"
-      @nonfin_a_blog = Blog.where(priority: 0).where(task_flag: false).page(params[:page]).per(3)
+      @nonfin_a_blog = Blog.where(priority: 0).where(user_id:current_user.id).where(task_flag: false).page(params[:page]).per(3)
       @blogs = @nonfin_a_blog
       @index_title = "優先度Aで未完了の投稿"
       render template: "blogs/nf_index"
     elsif params[:id] == "nonfin_B"
-      @nonfin_b_blog = Blog.where(priority: 1).where(task_flag: false).page(params[:page]).per(3)
+      @nonfin_b_blog = Blog.where(priority: 1).where(user_id:current_user.id).where(task_flag: false).page(params[:page]).per(3)
       @blogs = @nonfin_b_blog
       @index_title = "優先度Bで未完了の投稿"
       render template: "blogs/nf_index"
     elsif params[:id] == "nonfin_C"
-      @nonfin_c_blog = Blog.where(priority: 2).where(task_flag: false).page(params[:page]).per(3)
+      @nonfin_c_blog = Blog.where(priority: 2).where(user_id:current_user.id).where(task_flag: false).page(params[:page]).per(3)
       @blogs = @nonfin_c_blog
       @index_title = "優先度Cで未完了の投稿"
       render template: "blogs/nf_index"
